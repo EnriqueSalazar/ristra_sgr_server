3d7f38229883b8a2c4b328ab2dbf9e3128355c14
Enrique Salazar <enrique.salazar@gmail.com>
2018-12-02 11:08:40 +0000

    sequelize working

--- index.js
+++ index.js
@@ -6,30 +6,12 @@ const bodyParser = require('body-parser');
 const Sequelize = require('sequelize');
 const sequelize = new Sequelize('ristra_sgr', 'ristra_sgr', 'x;O961{7nR}u', {
 	host: 'ristra.co',
-	dialect: 'mysql',
-	operatorsAliases: false,
-
-	pool: {
-		max: 5,
-		min: 0,
-		acquire: 30000,
-		idle: 10000,
-	},
+	dialect: 'mysql'
 });
 const port = 8080;
 
-// app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors());
-
-// app.options('*', cors());
-
-// app.use(function(req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//   next();
-// });
-
 
 app.get('/', cors(), (req, res) => {
     console.log('get /');
@@ -39,14 +21,27 @@ app.post('/', (req, res) => {
   console.log(req.body.number);
 	res.send('hello post');
 });
-app.post('/data', (req, res) => {
+// app.post('/data', (req, res) => {
+//   console.log('post body', req.body);
+// res.send(req.body);
+// });
 
-  console.log('post body', req.body);
-res.send(req.body);
+const Questionario = sequelize.define('questionario', {
+  by: Sequelize.STRING,
+  data: Sequelize.STRING
 });
-app.get('/data', (req, res) => {
-  console.log('get', req.body);
-	res.sendStatus(200);
+
+app.post('/data', (req, res) => {
+  console.log('post data', req.body);
+  sequelize.sync()
+  .then(() => Questionario.create({
+    by: req.body.email,
+	data: JSON.stringify(req.body)
+  }))
+  .then(questionario => {
+    console.log(questionario.toJSON());
+	res.send(questionario.toJSON());
+  });
 });
 app.post('/email/', (req, res) => {
 	res.send(JSON.stringify(req.body.email + '_'));
